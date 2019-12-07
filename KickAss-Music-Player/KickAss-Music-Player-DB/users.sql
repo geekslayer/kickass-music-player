c2a12600cd4af941ff25aab7cdcb8af86dea920c
geekslayer <cymansys@live.com>
2019-12-07 14:04:00 -0500

    introduce the keycloak docker container to start with the authentication

---
+++ KickAss-Music-Player/KickAss-Music-Player-DB/users.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Username] VARCHAR(30) NOT NULL,
+	Password varchar(255) not null,
+    [Firstname] VARCHAR(50) NOT NULL, 
+    [Lastname] VARCHAR(50) NOT NULL,
+	[Active] BIT NOT NULL DEFAULT 0,
+    [CreatedBy] INT NOT NULL, 
+    [CreationDate] DATETIME2 NOT NULL, 
+    [ModifiedBy] NCHAR(10) NULL, 
+    [ModificationDate] DATETIME2 NULL
+)
+
+GO
+
+CREATE NONCLUSTERED INDEX [NIX_UsersUsername] ON [dbo].[users] ([Username]) include (Id, Active, Firstname, Lastname) WITH (ALLOW_PAGE_LOCKS=ON, ALLOW_ROW_LOCKS=ON, FILLFACTOR=90);
